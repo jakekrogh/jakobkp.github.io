9715910c27b21f51e9140e77e8b61092b0fc631f
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-26 19:08:45 +0200

    hej

--- js/djikstra.js
+++ js/djikstra.js
@@ -13,7 +13,7 @@ var drawGrid = function(){
             <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5" /> \
         </pattern> \
         <pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse"> \
-            <rect width="80" height="80" fill="url(#smallGrid)" /> \
+            <rect width="100" height="100" fill="url(#smallGrid)" /> \
             <path d="M 80 0 L 0 0 0 80" fill="none" stroke="gray" stroke-width="1" /> \
         </pattern> \
     </defs> \
