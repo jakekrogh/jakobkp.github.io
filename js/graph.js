4cc60c9f5a67de83c9c8f3ad57d1ea9d83962cce
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-26 21:49:40 +0200

    hej

--- js/graph.js
+++ js/graph.js
@@ -3,18 +3,20 @@ class Graph{
     this.nodes = new Map()
     this.edgeDirection = edgeDirection
   }
-  addVertex(value){
+  addVertex(x,y,value){
     if(this.nodes.has(value)){
       return this.nodes.get(value)
+    } else if (x == -1 && y == -1) {
+        return null;
     } else{
-      const vertex = new Node(value)
+      const vertex = new Node(x,y,value)
       this.nodes.set(value,vertex)
       return vertex
     }
   }
   addEdge(source,destination,weight){
-    const sourceNode = this.addVertex(source)
-    const destNode = this.addVertex(destination)
+    const sourceNode = this.addVertex(-1,-1,source)
+    const destNode = this.addVertex(-1,-1,destination)
 
     sourceNode.addAdjacent(destNode,weight)
 
