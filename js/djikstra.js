2878db24edf917fcd3247470a7d0f235fbd63459
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-26 18:20:06 +0200

    hej

--- js/djikstra.js
+++ js/djikstra.js
@@ -17,7 +17,29 @@ function init () {
   // }
 }
 
+var drawGrid = function(w, h, id) {
+  var canvas = document.getElementById(id);
+  var ctx = canvas.getContext('2d');
+  ctx.canvas.width  = w;
+  ctx.canvas.height = h;
+
+
+  for (x=0;x<=w;x+=20) {
+      for (y=0;y<=h;y+=20) {
+          ctx.moveTo(x, 0);
+          ctx.lineTo(x, h);
+          ctx.stroke();
+          ctx.moveTo(0, y);
+          ctx.lineTo(w, y);
+          ctx.stroke();
+      }
+  }
+
+};
+
 // wait for the HTML to load
 document.addEventListener('DOMContentLoaded', init)
 
 console.log(3+2)
+
+drawGrid(500,500,'canv')
