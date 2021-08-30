37c465806e0da15f81f84c556a56b6a7aae35cae
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-30 20:03:26 +0200

    refracture

--- js/djikstra.js
+++ js/djikstra.js
@@ -66,9 +66,8 @@ canvas.addEventListener('contextmenu', function(event){
   console.log(inCircle)
   let minIndex = indexofMin(inCircle)
   const iter = graph.nodes.entries()
-  let minVal = -1
-  for (i = 0; i <= indexofMin; i++){
-    minVal = iter.next().value[0]
+  for (i = 0; i <= minIndex; i++){
+    let minVal = iter.next().value[0]
   }
   let remNode = graph.nodes.get(minVal).circ
   ctx.beginPath()
