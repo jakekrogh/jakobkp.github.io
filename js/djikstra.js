7936834f01103d27c17c1b2aa4650bca028a23cd
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-30 17:38:13 +0200

    refracture

--- js/djikstra.js
+++ js/djikstra.js
@@ -60,8 +60,8 @@ canvas.addEventListener('contextmenu', function(event){
   var pointx = (event.pageX - canvLeft)
   var pointy = (event.pageY - canvTop)
   let inCircle = []
-  console.log("graph length:" + graph.nodes.length)
-  for (var i = 0; i < graph.nodes.length; i++){
+  console.log("graph length:" + graph.nodes.size)
+  for (var i = 0; i < graph.nodes.size; i++){
     console.log("we get in for loop")
     let val = (pointx**2+pointy**2-(graph.nodes[i,1].circ.rad)**2)
     console.log(val)
