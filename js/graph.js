becdcd70eb736c89302fc1b2b7fc584a496330fa
jakekrogh <55795618+jakekrogh@users.noreply.github.com>
2021-08-30 16:33:21 +0200

    refracture

--- js/graph.js
+++ js/graph.js
@@ -6,7 +6,7 @@ class Graph{
   addVertex(x,y,value){
     if(this.nodes.has(value)){
       return this.nodes.get(value)
-    } else{
+    } else {
       const vertex = new Node(x,y,value)
       this.nodes.set(value,vertex)
       return vertex
@@ -14,12 +14,11 @@ class Graph{
   }
   addEdge(source,destination,weight){
       sourceNode.addAdjacent(destNode,weight)
-
       if (this.edgeDirection == Graph.UNDIRECTED){
         destNode.addAdjacent(sourceNode,weight)
       }
       return [sourceNode, destNode, weight]
   }
 }
-Graph.UNDIRECTED = Symbol('directed graph'); // one-way edges
-Graph.DIRECTED = Symbol('undirected graph'); // two-ways edges
+Graph.UNDIRECTED = Symbol('directed graph'); // one-ways edges
+Graph.DIRECTED = Symbol('undirected graph'); // two-way edges
